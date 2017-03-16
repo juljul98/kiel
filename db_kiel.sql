1bf210810eb4db9e33fb9980894766f007e3f81d
ph2150091 <ph2150091@transcosmos-ph.local>
2017-03-16 15:51:16 +0800

    dtb

--- db_kiel.sql
+++ db_kiel.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 15, 2017 at 09:55 AM
+-- Generation Time: Mar 16, 2017 at 08:50 AM
 -- Server version: 10.1.9-MariaDB
 -- PHP Version: 5.6.15
 
@@ -28,7 +28,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` int(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `middlename` varchar(255) NOT NULL,
@@ -37,15 +39,15 @@ CREATE TABLE `users` (
   `address` varchar(255) NOT NULL,
   `birthday` varchar(255) NOT NULL,
   `date_created` varchar(255) NOT NULL,
-  `date_update` varchar(255) NOT NULL
+  `date_updated` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `firstname`, `middlename`, `lastname`, `gender`, `address`, `birthday`, `date_created`, `date_update`) VALUES
-(1, 'juls', '21232f297a57a5a743894a0e4a801fc3', '', '', '', '', '', '', '', '');
+INSERT INTO `users` (`id`, `image`, `username`, `email`, `password`, `firstname`, `middlename`, `lastname`, `gender`, `address`, `birthday`, `date_created`, `date_updated`) VALUES
+(12, '', 'juls', 'mateojulius98@yahoo.com', '2a0c72ddb49cc98625162339d270ab58', 'Julius', 'Delos Santos', 'Mateo', 'male', 'Kaingin St. Balasing Sta. Maria Bulacan', 'jan 1 1990', '2017-03-16 06:28:04', '2017-03-16 06:28:04');
 
 --
 -- Indexes for dumped tables
@@ -65,7 +67,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
